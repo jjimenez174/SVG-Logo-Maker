f1c12e765b1d9d82a711fff4cd42467e892a5432
Jessica Jimenez <jjimenez174@yahoo.com>
2024-03-13 18:12:38 -0700

    created shape.js, index. js and shapes.test.jest

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # SVG-Logo-Maker
+
+## Description
+Create an SVG logo with your choice of a shape (circle, triangle, or square), color and text. 
+
+[Link to Demo]()
+
+## Table of Content
+[Installation](#installation)  
+[Usage](#usage)  
+[Contribution](#contribution)  
+[Test](#test)  
+[Questions](#questions) 
+
+## Installation
+* Clone the repositoy
+* Open the file in VS code or any source code editor
+* Open terminal
+* run <b>npm init</b> in terminal
+* run <b>npm i inquirer@8.2.4</b> in the terminal
+* run <b>npm i jest</b> in the terminal
+
+All set to run the app. 
+
+## Usage
+ * run <b>node index</b> in the terminal
+ * answer the prompt questions
+ * a logo would be generated in the Design folder
+
+## Contribution
+At the moment I won't be accepting contributions.
+
+## Test
+The three shapes are being tested for the render() method. Where it returns a string that matches the color and text of the SVG file. Run <b> npm test</b> in the terminal and all three test will run. 
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+For more information, visit [License Details](https://opensource.org/license/MIT)
+
+## Questions
+
+Contact me at: [jjimenez174](https://github.com/jjimenez174)
---
+++ index.js
@@ -0,0 +1,63 @@
+// Packages for the application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Circle } = require('./lib/circle');
+const { Triangle } = require('./lib/triangle');
+const { Square } = require('./lib/square');
+
+//An array of questions 
+ const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What is the logo name? (three characters only)',
+        validate: (answer) => {
+            if (answer.length > 3) {
+                console.log('\n Text must be three characters or less. Please try again');
+                return false;
+            }
+            return true;
+        }
+}, {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (color keyword OR hexadecimal number)',
+ }, {
+        type: 'list',
+        name: 'shape',
+        message: 'Please choose a shape',
+        choices: ['Circle', 'Triangle', 'Square']
+ }, {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter color of shape (color keyword OR hexadecimal number)',
+ },
+];
+
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            console.log(response)
+            let selectedShape;
+            if (response.shape === 'Square') {
+                selectedShape = new Square(response.text, response.textColor, response.shapeColor );
+            } else if (response.shape === 'Circle') {
+                selectedShape = new Circle(response.text, response.textColor, response.shapeColor );
+            } else {
+                selectedShape = new Triangle(response.text, response.textColor, response.shapeColor);
+            }
+
+            fs.writeFile('./Design/logo.svg', selectedShape.render(), (error) => {
+                if (error) {
+                    console.error('Error writing to file:', error);
+                } else {
+                    console.log('Shape successfully written to logo.svg');
+                }
+            });
+        })
+        .catch((err) => {
+            console.error('An error occurred:', err);
+        });
+}
+
+init();
